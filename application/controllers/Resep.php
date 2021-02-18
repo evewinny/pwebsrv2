195ea38bd52ad240744331c2d5e77111f1d5a386
evewinny <evewinny10@gmail.com>
2021-02-18 15:48:36 +0000

    fira wibu

---
+++ application/controllers/Resep.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+class Resep extends CI_Controller
+{
+
+	public function index()
+	{
+	 $this->load->view('resep/list');
+	}
+
+	public function palbas()
+	{
+	 $this->load->view('resep/pallubasa');
+	}
+}
+?>
